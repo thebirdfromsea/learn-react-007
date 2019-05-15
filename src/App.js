62995790147c94d9656d6d0451fff901e702cc39
thebirdfromsea <46797898+thebirdfromsea@users.noreply.github.com>
2019-05-14 21:32:40 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import qa from './qa';
+class Accordin extends React.Component{
+    render(){
+        return (
+            <div>
+                Accordin goes here
+            </div>
+        );
+    }
 }
-
-export default App;
+export default Accordin;
---
+++ src/qa.js
@@ -0,0 +1,14 @@
+const qa = [
+    {
+        question: "What is react?",
+        answer:
+        "React is a JavaScript library used to build views which are mainl"
+    },
+    {
+        question: "What is JSX ?",
+        answer:
+        "JSX is JavaScript XML which is used inside the react to write the HTML like syntax inside the javascript it’s just an preprocessor"
+       
+    },
+]
+export default qa;
